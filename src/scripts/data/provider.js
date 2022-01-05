246f9eca1a032dbc2c5e3ef55cbf1f72a1be3ec5
Austin Davis <addavis191@gmail.com>
2022-01-05 13:44:35 -0600

    added fetch calls

--- src/scripts/data/provider.js
+++ src/scripts/data/provider.js
@@ -19,12 +19,84 @@ export const fetchUsers = () => {
     return fetch(`${API}/users`)
         .then(response => response.json())
         .then(
-            (users) => {
-                applicationState.users = users
+            (user) => {
+                applicationState.users = user
             }
         )
 }
 
 export const getUsers = () => {
-    return applicationState.chosenUsers.map(users => ({ ...users }))
+    return applicationState.users.map(user => ({ ...user}))
 }
+
+export const login = (userLogin) => {
+    const fetchOptions = {
+        method: "GET", 
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(userLogin)
+    }
+return fetch(`${API}/users`, fetchOptions)
+.then(response => response.json())
+.then(
+    () => {
+        mainContainer.dispatchEvent(new CustomEvent("stateChanged"))
+    }
+)
+}
+
+
+// export const fetchPosts = () => {
+//     return fetch(`${API}/posts`)
+//     .then(response => response.json())
+//     .then(
+//         (posts) => {
+//             applicationState.posts = posts
+//         }
+//     )
+//  }
+
+//   export const getPosts = () => {
+//     return applicationState.posts.map( (post) => ({...post}))
+//  }
+ 
+//   export const fetchLikes = () => {
+//     return fetch(`${API}/likes`)
+//     .then(response => response.json)
+//     .then(
+//         (likes) => {
+//             applicationState.likes = likes
+//         }
+//     )
+//  }
+//   export const getLikes = () => {
+//     return applicationState.likes.map( (like) => ({...like}))
+//  }
+//   export const fetchFollows = () => {
+//     return fetch(`${API}/follows`)
+//     .then(response => response.json)
+//     .then(
+//           (follows) => {
+//               applicationState.follows = follows
+//           }
+//     )
+//  }
+//   export const getFollows = () => {
+//     return applicationState.follows.map( (follow) => ({...follow}))
+//  }
+//   export const fetchMessages = () => {
+//     return fetch(`${API}/messages`)
+//     .then(response => response.json)
+//     .then(
+//         (messages) => {
+//             applicationState.messages = messages
+//         }
+//     )
+//  }
+//   export const getMessages = () => {
+//     return applicationState.messages.map( (message) => ({...message}))
+//  }
+ 
+ 
+ 
