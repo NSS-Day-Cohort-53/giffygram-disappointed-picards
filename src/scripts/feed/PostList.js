e3bbbb43be6525c744b6464ae7a7a9ead1d97b5a
Jennifer-Blue-Williams <jennifer.blue.williams@gmail.com>
2022-01-06 13:35:01 -0600

    added to postList

---
+++ src/scripts/feed/PostList.js
@@ -0,0 +1,48 @@
+import { getPosts } from "./data/provider.js"
+
+export const Posts = () => {
+    const posts = getPosts()
+    const postHtml = (post) => {
+ 
+        let postsList = ""
+
+        if ( posts.length > 0 ) {
+          postsList += `<ul>`
+    const postsArray = posts.map( (post) => {     
+              
+    const foundDescription = posts.description.find((description) => description === post.userId)   
+    
+    const foundTitle = posts.title.find((title) => title === post.userId)   
+    
+    const foundImg = posts.img.find((img) => img === post.userId)   
+     
+              
+    //         return `
+    //     <li>
+    //         ${fetchPosts}
+            
+    //         <button class="request__delete"
+    //                 id="post--${post.id}">
+    //             Delete
+    //         </button>
+    //     </li>
+    // `    
+
+    return `
+    <ul post--list-item">
+      post #${post.id} placed on ${new Date(post.timestamp).toLocaleString()},
+    //    ${foundDescription.posts} ${foundTitle.type} 
+    //   ${foundToppings.map((topping) => topping.name).join(" and ")}
+    //   <p>
+    //   description: $${postPrice.toFixed(2)}
+    //   </p> 
+    </ul>
+    <button name="deletepost" id='${post.id}'>Delete post</button>
+    `
+})
+
+postsList += postsArray.join("")
+postsList += `</ul>`
+}
+
+return postsList 
