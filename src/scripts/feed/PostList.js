907d2badba608096bc55c8df59d7b91962fb9ff8
Jennifer-Blue-Williams <jennifer.blue.williams@gmail.com>
2022-01-06 14:31:28 -0600

    rendering posts

--- src/scripts/feed/PostList.js
+++ src/scripts/feed/PostList.js
@@ -1,48 +1,35 @@
-import { getPosts } from "./data/provider.js"
+import { getPosts } from "../data/provider.js"
 
 export const Posts = () => {
     const posts = getPosts()
-    const postHtml = (post) => {
- 
-        let postsList = ""
+    console.log(posts)
 
-        if ( posts.length > 0 ) {
-          postsList += `<ul>`
-    const postsArray = posts.map( (post) => {     
-              
-    const foundDescription = posts.description.find((description) => description === post.userId)   
+//     let postsList = ""
     
-    const foundTitle = posts.title.find((title) => title === post.userId)   
-    
-    const foundImg = posts.img.find((img) => img === post.userId)   
-     
+//     if ( posts.length > 0 ) {
+//         postsList += `<ul>`
+//     const postsArray = posts.map( (post) => {     
               
-    //         return `
-    //     <li>
-    //         ${fetchPosts}
-            
-    //         <button class="request__delete"
-    //                 id="post--${post.id}">
-    //             Delete
-    //         </button>
-    //     </li>
-    // `    
+//     const foundDescription = posts.description.find((description) => description === post.userId)   
+    
+//     const foundTitle = posts.title.find((title) => title === post.userId)   
+    
+//     const foundImg = posts.img.find((img) => img === post.userId)   
 
-    return `
-    <ul post--list-item">
-      post #${post.id} placed on ${new Date(post.timestamp).toLocaleString()},
-    //    ${foundDescription.posts} ${foundTitle.type} 
-    //   ${foundToppings.map((topping) => topping.name).join(" and ")}
-    //   <p>
-    //   description: $${postPrice.toFixed(2)}
-    //   </p> 
-    </ul>
-    <button name="deletepost" id='${post.id}'>Delete post</button>
-    `
-})
+//     return `
+//     <ul post--list-item">
+//       post #${post.id} placed on ${new Date(post.timestamp).toLocaleString()},
+//     //    ${foundDescription.posts} ${foundTitle.type} 
+//     //   ${foundToppings.map((topping) => topping.name).join(" and ")}
+//     //   <p>
+//     //   description: $${postPrice.toFixed(2)}
+//     //   </p> 
+//     </ul>
+//     <button name="deletepost" id='${post.id}'>Delete post</button>
+//     `
+// })
 
-postsList += postsArray.join("")
-postsList += `</ul>`
+// postsList += postsArray.join("")
+// postsList += `</ul>`
+// }
 }
-
-return postsList 
